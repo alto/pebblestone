e3351eeb9833f007d5afcba971f0f88a13ac3599
Thorsten Böttger <alto@mt7.de>
2011-03-28 18:47:33 +0200

    Rails app based on MySql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pebblestone::Application.config.secret_token = '0eec5494a29a07d37dcd898ed3a2f59962c5fcab60c6a1453315ebed8f1870ea28de7e3d3c3b214f84134b2757381eb51434b6ccb5baeed602d76baece3e995c'
