6a1555513d5e66187df9dddfa815e5fe99c19a52
Thorsten Böttger <alto@mt7.de>
2011-09-04 15:49:28 +0200

    got it running on mac and ipad

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,29 +1,51 @@
+function log() { if(typeof console !== "undefined" && console && console.log) {console.log(arguments);};}
+
 $(document).ready(function() {
+
+  function draw(x,y) {
+    // console.log("drawing(" + x + "," + y + ")");
+    ctx.lineWidth = 1;
+    // ctx.lineCap = "round";
+    // ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x+1, y+1);
+    // ctx.closePath();
+    ctx.strokeStyle = "#000"
+    ctx.stroke();
+  };
+
   var ctx = document.getElementById('canvas').getContext("2d");
   ctx.strokeStyle = 'black';
-  var draw = false;
+  var drawing = false;
   
-  $("#canvas").mousedown(function() {draw=true;});
-  $("#canvas").mouseup(function() {draw=false;});
-  $("#canvas").bind("mousemove", function(e) {
-    if(draw == true){
-      var x = e.pageX;
-      var y = e.pageY;
-      console.log("drawing(" + x + "," + y + ")");
-      ctx.lineWidth = 1;
-      // ctx.lineCap = "round";
-      // ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.lineTo(x+1, y+1);
-      // ctx.closePath();
-      ctx.strokeStyle = "#000"
-      ctx.stroke();
+  var canvas = $("#canvas")
+  canvas.bind("mousedown", function(e) {drawing=true;});
+  canvas.bind("mouseup", function(e) {drawing=false;});
+
+  canvas.bind("mousemove", function(e) {
+    e.preventDefault();
+    if(drawing == true){
+      var x, y;
+      if (e.offsetX) {
+        x = e.offsetX;
+        y = e.offsetY;
+      } else if (e.layerX) {
+        x = e.layerX;
+        y = e.layerY;        
+      } else {
+        x = e.pageX - canvas.offsetLeft;
+        y = e.pageY - canvas.offsetTop;
+      }
+      draw(x,y);
     }    
   });
 
-  window.onresize = function() {
-    var canvas = $("#canvas")
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-  };
+  canvas.bind("touchmove", function(e) {
+    e.preventDefault();
+    var x, y;
+    x = e.originalEvent.touches[0].pageX;
+    y = e.originalEvent.touches[0].pageY;
+    draw(x,y);
+  });
+
 });
